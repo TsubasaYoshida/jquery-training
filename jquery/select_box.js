a2e632da34e7675d94916a589a9c6b3686ad0df9
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-05-23 00:00:02 +0900

    セレクトボックスを連動させてみる

--- jquery/select_box.js
+++ jquery/select_box.js
@@ -1,10 +1,28 @@
 $(function(){
-  const selectBoxValue = $('.select-box-value')
-  const selectBoxValueOption = selectBoxValue.find('option');
+  const selectBoxValue = $('.select-box-value');
+  const vegetables = [
+    'にんじん',
+    'はくさい',
+    'じゃがいも'
+  ];
+  const fruits = [
+    'りんご',
+    'ばなな',
+    'めろん'
+  ];
 
   $('.select-box-type').change(function(){
-    const val = $(this).val();
+    const foodType = $(this).val();
     selectBoxValue.empty();
+    if (foodType === 'くだもの') {
+      for (let fruit of fruits) {
+        selectBoxValue.append($("<option>").val(fruit).text(fruit));
+      }
+    }
+    if (foodType === 'やさい') {
+      for (let vegetable of vegetables) {
+        selectBoxValue.append($("<option>").val(vegetable).text(vegetable));
+      }
+    }
   });
-
 });
